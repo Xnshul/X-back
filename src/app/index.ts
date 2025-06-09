15c490b8ca76f0ea3a9b6e0eed525a8dc8b4a763
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-06-09 17:32:16 +0530

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,35 +1,28 @@
-<<<<<<< HEAD
-import express, { query } from "express";
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@apollo/server/express4";
-import bodyParser from "body-parser";
-
-export async function initServer() {
-  const app = express();
-  app.use(bodyParser.json({}))
-  const graphqlServer = new ApolloServer({
-     typeDefs: `type Query { sayHello: String }`,
-     resolvers:{
-          Query:{
-               sayHello:() => `Hello From graph Sql Server`
-          }
-     }
-
-  });
-
- await graphqlServer.start();
- app.use("/graphql", expressMiddleware(graphqlServer));
- return app;
-}
-=======
 import express = require("express");
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-import * as bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import { printSchema } from "graphql";
+import { PrismaClient } from "@prisma/client";
 
 export async function initServer(){
   const app = express();
-  app.use(bodyParser.json())
+  app.use(bodyParser.json());
+  const graphqlServer = new ApolloServer({
+    typeDefs:`type Query{
+    sayHello: String
+    sayHelloToMe(name: String!):String}`,
+    resolvers: {
+      Query: {
+        sayHello: () => `Hello From Graphql Server`,
+        sayHelloToMe: (parent: any,{name}: {name:string}) => null,
+      },
+    },
+  });
+  await graphqlServer.start();
+ app.use("/graphql", expressMiddleware(graphqlServer));
+
+
+
+  return app;
 }
->>>>>>> d432332 (googleauth)
